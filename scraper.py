5bbc8bd28f84598a3c224984e104190854547f27
nels2248 <nels2248@gmail.com>
2026-04-30 17:57:25 -0500

    set incremental to 24 hours

--- scraper.py
+++ scraper.py
@@ -242,7 +242,7 @@ def scrape_incremental(con):
 
     since = (
         datetime.now(timezone.utc)
-        - timedelta(hours=72)
+        - timedelta(hours=24)
     ).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     log.info(f"MODE: INCREMENTAL since {since}")
